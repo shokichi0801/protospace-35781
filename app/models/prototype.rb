d9f114a6c22678dc808b90f89b3dfa10d4d241b7
shokichi0801 <85923227+shokichi0801@users.noreply.github.com>
2021-08-23 17:06:09 +0900

    コメント機能の実装

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,9 +1,10 @@
 class Prototype < ApplicationRecord
-  validates :title, presence: true
-  validates :catch_copy, presence: true
-  validates :concept, presence: true
   belongs_to :user
   has_one_attached :image
-  validates :image, presence: true
   has_many :comments, dependent: :destroy
+
+  validates :title, presence: true
+  validates :catch_copy, presence: true
+  validates :concept, presence: true  
+  validates :image, presence: true
 end
